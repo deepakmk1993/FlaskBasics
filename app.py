24f0449aa0bfc665d347fdb1a5c4e3da66782135
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-11 17:42:51 +0000

    first commit

--- app.py
+++ app.py
@@ -19,6 +19,4 @@ def add(num1, num2):
     return render_template("add.html", **context)
 
 if __name__ == "__main__":
-    host = os.environ.get('IP', '0.0.0.0')
-    port = int(os.environ.get('PORT', 8080))
-    app.run(debug=False, host=host, port=port)
+    app.run()
